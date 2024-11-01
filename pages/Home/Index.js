a426c07baa3437a1797664c25cfd5e9fb0e036cd
Thnorty <ototatik@gmail.com>
2024-11-01 20:36:16 +0300

    first commit

---
+++ pages/Home/Index.js
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import {useNavigation} from "@react-navigation/native";
+
+const Index = (props) => {
+	const navigation = useNavigation();
+
+	return (
+		<View>
+		</View>
+	);
+};
+
+export default Index;
---
+++ pages/Medicines/Index.js
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+const Index = (props) => {
+	return (
+		<View>
+			<Text style={{color: "black"}}>Home Page</Text>
+		</View>
+	);
+};
+
+export default Index;
