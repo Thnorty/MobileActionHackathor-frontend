5e8e7f0afd645ece77989f249a937007395457da
Thnorty <ototatik@gmail.com>
2024-11-02 01:50:30 +0300

    added doctor calling function

--- pages/Home/Index.js
+++ pages/Home/Index.js
@@ -3,11 +3,13 @@ import {useNavigation} from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import {useTranslation} from "react-i18next";
 import {useState, useEffect} from "react";
+import backend from "../../utils/Backend";
 
 const Index = () => {
 	const {t} = useTranslation();
 	const navigation = useNavigation();
 	const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+	const [doctorPhone, setDoctorPhone] = useState("");
 	const [emergencyPhone, setEmergencyPhone] = useState("");
 
 	useEffect(() => {
@@ -18,11 +20,19 @@ const Index = () => {
 		.then((response) => {
 			setEmergencyPhone(response.data.phone);
 		});
+		backend.post("/api/doctor_phone/", payload)
+		.then((response) => {
+			setDoctorPhone(response.data.phone);
+		});
 		const timer = setInterval(() => {
 			setCurrentTime(new Date().toLocaleTimeString());
 		}, 1000);
 		return () => clearInterval(timer);
 	}, []);
+
+	const handleCallDoctor = () => {
+		Linking.openURL("tel:" + doctorPhone);
+	}
 
 	const handleEmergencyCall = () => {
 		Linking.openURL("tel:" + emergencyPhone);
@@ -31,7 +41,6 @@ const Index = () => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.timeText}>{currentTime}</Text>
-			
 			<TouchableOpacity style={[styles.button, {backgroundColor: "#06bae3"}]} onPress={() => navigation.navigate("Medicines")}>
 				<Icon name="medkit" size={30} color="white" />
 				<Text style={styles.buttonText}>{t("Medicines")}</Text>
@@ -39,6 +48,10 @@ const Index = () => {
 			<TouchableOpacity style={[styles.button, {backgroundColor: "#06e35e"}]} onPress={() => navigation.navigate("Appointments")}>
 				<Icon name="calendar" size={30} color="white" />
 				<Text style={styles.buttonText}>{t("Appointments")}</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={[styles.button, {backgroundColor: "#a134eb"}]} onPress={handleCallDoctor}>
+				<Icon name="phone" size={30} color="white" />
+				<Text style={styles.buttonText}>{t("ConsultDoctor")}</Text>
 			</TouchableOpacity>
 			<TouchableOpacity style={[styles.button, {backgroundColor: "#e30606"}]} onPress={handleEmergencyCall}>
 				<Icon name="phone" size={30} color="white" />
